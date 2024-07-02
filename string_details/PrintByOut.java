9d091cf95e5f5e91e8dc4d5a99a456152a68a18f
shubham-alavni <shubham.alavni@gmail.com>
2024-07-02 11:31:55 +0530

    chore: Fix print statements in PrintByOut class

---
+++ string_details/PrintByOut.java
@@ -0,0 +1,15 @@
+package string_details;
+
+/**
+ * PrintByOut
+ */
+public class PrintByOut {
+
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+    System.out.println("Sum is " + x + y); // Sum is 1020
+    System.out.println("Sum is " + (x + y)); // Sum is 30
+    System.out.println("Sum of " + x + " and " + y + " is " + x + y); // Sum of 10 and 20 is 1020
+  }
+}
